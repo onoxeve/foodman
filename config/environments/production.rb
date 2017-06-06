b2d309c85c46dd8595317214c9e283ba112aebfa
onoxeve <onoxeve@gmail.com>
2017-06-06 11:15:09 +0000

    update actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # For AWS
+  config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
 end
