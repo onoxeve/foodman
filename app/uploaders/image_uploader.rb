aa75037efe124533c322f9f241c3d68b0917f2ab
onoxeve <onoxeve@gmail.com>
2017-06-12 23:51:01 +0900

    add mail update fogaws

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   process :fix_rotate
- 
+
   def fix_rotate
     manipulate! do |img|
       img = img.auto_orient
@@ -21,7 +21,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,20 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # for sending mail
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'email-smtp.us-west-2.amazonaws.com',
+    :port => 587,
+    :authetication => :login,
+    :user_name => ENV['ses_api_key'],
+    :domain => "localhost:3000",
+    :password => ENV['ses_api_secret'],
+    #:ssl => true,
+    #:tls => true,
+    :enable_starttls_auto => true,
+  }
 end
