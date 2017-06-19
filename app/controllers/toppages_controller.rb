3e17fb3b406d8fa151f6167bd2e6c506b37924ff
onoxeve <onoxeve@gmail.com>
2017-06-19 20:21:21 +0900

    update toppage

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,9 +3,5 @@ class ToppagesController < ApplicationController
   def index
     @food = Food.new
     @foods = Food.order('updated_at DESC').page(params[:page]).per(10)
-    #if user_signed_in?
-    #   @user = current_user
-    #end
-    #binding.pry
   end
 end
