af5842e50cfb0bcacf684df2bbe0e5afc22636c8
onoxeve <onoxeve@gmail.com>
2017-06-22 20:22:45 +0900

    update food uploader

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,7 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
+  before_destroy :clean_s3
 
   validates :user_id, presence: true
   validates :image, presence: true
@@ -9,4 +10,11 @@ class Food < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   after_create_commit { FoodBroadcastJob.perform_later self }
+
+  private
+
+  def clean_s3
+    image.remove!
+    image.thumb.remove!
+  end
 end
